e5ca27fbbac1d618065d93b922a7943033586d44
Jeff M <jkdmarks@gmail.com>
2019-03-13 00:28:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |name, value|
     if name_hash.empty?
-      nil
+      min_name = nil
     elsif !min_value
       min_value = value
       min_name = name
